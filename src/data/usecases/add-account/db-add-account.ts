5d624744332fa85e03a52ef8751a8a5967403b6a
Daniel Castilho <dan.castilho@gmail.com>
2022-02-05 00:22:15 -0500

    refactor: rename Encrypter to Hasher

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,16 +1,16 @@
-import { AddAccount, AddAccountModel, Encrypter, AddAccountRepository, AccountModel } from './db-add-account-protocols'
+import { AddAccount, AddAccountModel, Hasher, AddAccountRepository, AccountModel } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
-  private readonly encrypter: Encrypter
+  private readonly hasher: Hasher
   private readonly addAccountRepository: AddAccountRepository
 
-  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
+  constructor (hasher: Hasher, addAccountRepository: AddAccountRepository) {
     this.addAccountRepository = addAccountRepository
-    this.encrypter = encrypter
+    this.hasher = hasher
   }
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const hashedPassword = await this.encrypter.encrypt(accountData.password)
+    const hashedPassword = await this.hasher.hash(accountData.password)
     const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
     return account
   }
