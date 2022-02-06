4a87abbc36cd2f1adb0825d615f43c2b879ed5c8
Daniel Castilho <dan.castilho@gmail.com>
2022-02-06 01:52:47 -0500

    feat: ensure Account return an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,12 +1,19 @@
-import { AddAccountModel } from '@/domain/usecases/add-account'
-import { AddAccountRepository } from '@/data/protocols/db/add-account-repository'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
-import { AccountModel } from '@/domain/models/account'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     return MongoHelper.map(result.insertedId, accountData)
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+    return MongoHelper.map(account._id, account)
   }
 }
